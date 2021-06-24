403ec9ca015cefc679f2509a4e196df2a2b214e8
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-06-24 19:25:32 +0100

    Final Commit

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <header id="site_header" class="bg_lightblue">
         <img src="./dist/img/logo_spotify.png" width="70" alt="">
         <div>
-                <a href="http://localhost/php/php-ajax-dischi/index.php">PHP</a>
-                <a href="http://localhost/php/php-ajax-dischi/vue.php">VUEJS</a>
+                <a href="./index.php">PHP</a>
+                <a href="./vue.php">VUEJS</a>
         </div>
 </header>
