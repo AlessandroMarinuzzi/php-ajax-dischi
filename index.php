edd397dd3219b67192954a81d14fe15eea200429
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-06-24 14:52:21 +0100

    -Verify file index is correctly linked to css & js

--- index.php
+++ index.php
@@ -73,3 +73,24 @@ $disks = [
         ]
     ];
 ?>
+
+<!-- HTML -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Link my app.css -->
+    <link rel="stylesheet" href="./dist/css/app.css">
+    <title>PHP Disks</title>
+</head>
+<body>
+    <header>
+        <h1>bella</h1>
+    </header>
+    
+    <!-- Script my app.js -->
+    <script src="./dist/js/app.js"></script>
+</body>
+</html>
